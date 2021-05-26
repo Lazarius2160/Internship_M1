0611150dff2a47959386c21bf91efa2599b580c3
Lazarius2160 <76939787+Lazarius2160@users.noreply.github.com>
2021-05-26 17:04:03 +0200

    BUILD_VTK: quadbuffer works on one render window

--- renderWindowCrystal.cxx
+++ renderWindowCrystal.cxx
@@ -1,7 +1,7 @@
-/* QuadBuffer using a render window (not qvtkstereowidget!) 
+/* QuadBuffer using a render window (not qvtkstereowidget!) ad a render window interactor
 Only in VTK, later in Slicer
 Use the examples in VTK examples cxx hello world with a cylinder
-*/ 
+*/
 
 
 #include <vtkActor.h>
@@ -14,70 +14,73 @@ Use the examples in VTK examples cxx hello world with a cylinder
 #include <vtkRenderWindow.h>    //Marine, ainsi on utilise renderwindow et pas le widget
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
-//Marine, pas besoin de nouveaux includes je pense 
-
+#include <vtkOpenGLRenderWindow.h> //Marine, a voir si besoin de ces fonctions
 
 #include <array>
 
-int main(int, char*[])
+int main(int, char* [])
 {
-  vtkNew<vtkNamedColors> colors;
+    vtkNew<vtkNamedColors> colors;
 
-  // Set the background color.
-  std::array<unsigned char, 4> bkg{{26, 51, 102, 255}};
-  colors->SetColor("BkgColor", bkg.data());
+    // Set the background color.
+    std::array<unsigned char, 4> bkg{ {26, 51, 102, 255} };
+    colors->SetColor("BkgColor", bkg.data());
 
-  // This creates a polygonal cylinder model with eight circumferential facets
-  // (i.e, in practice an octagonal prism).
-  vtkNew<vtkCylinderSource> cylinder;
-  cylinder->SetResolution(8);
+    // This creates a polygonal cylinder model with eight circumferential facets
+    // (i.e, in practice an octagonal prism).
+    vtkNew<vtkCylinderSource> cylinder;
+    cylinder->SetResolution(8);
 
-  // The mapper is responsible for pushing the geometry into the graphics
-  // library. It may also do color mapping, if scalars or other attributes are
-  // defined.
-  vtkNew<vtkPolyDataMapper> cylinderMapper;
-  cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
+    // The mapper is responsible for pushing the geometry into the graphics
+    // library. It may also do color mapping, if scalars or other attributes are
+    // defined.
+    vtkNew<vtkPolyDataMapper> cylinderMapper;
+    cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
 
-  // The actor is a grouping mechanism: besides the geometry (mapper), it
-  // also has a property, transformation matrix, and/or texture map.
-  // Here we set its color and rotate it around the X and Y axes.
-  vtkNew<vtkActor> cylinderActor;
-  cylinderActor->SetMapper(cylinderMapper);
-  cylinderActor->GetProperty()->SetColor(
-      colors->GetColor4d("Tomato").GetData());
-  cylinderActor->RotateX(30.0);
-  cylinderActor->RotateY(-45.0);
+    // The actor is a grouping mechanism: besides the geometry (mapper), it
+    // also has a property, transformation matrix, and/or texture map.
+    // Here we set its color and rotate it around the X and Y axes.
+    vtkNew<vtkActor> cylinderActor;
+    cylinderActor->SetMapper(cylinderMapper);
+    cylinderActor->GetProperty()->SetColor(
+        colors->GetColor4d("Tomato").GetData());
+    cylinderActor->RotateX(30.0);
+    cylinderActor->RotateY(-45.0);
 
-  // The renderer generates the image
-  // which is then displayed on the render window.
-  // It can be thought of as a scene to which the actor is added
-  vtkNew<vtkRenderer> renderer;
-  renderer->AddActor(cylinderActor);
-  renderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
-  // Zoom in a little by accessing the camera and invoking its "Zoom" method.
-  renderer->ResetCamera();
-  renderer->GetActiveCamera()->Zoom(1.5);
+    // The renderer generates the image
+    // which is then displayed on the render window.
+    // It can be thought of as a scene to which the actor is added
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(cylinderActor);
+    renderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
+    // Zoom in a little by accessing the camera and invoking its "Zoom" method.
+    renderer->ResetCamera();
+    renderer->GetActiveCamera()->Zoom(1.5);
 
-  // The render window is the actual GUI window
-  // that appears on the computer screen
-  vtkNew<vtkRenderWindow> renderWindow;
-  renderWindow->SetStereoCapableWindow(1);     //Marine, vtkbool true =1 ??
-  renderWindow->SetStereoTypeToCrystalEyes();    //Marine
-  renderWindow->SetStereoRender(1);     //Marine, normalement 1 = crystal eyes dans renderwindow, besoin de déclarer ici ??
-  renderWindow->SetSize(300, 300);
-  renderWindow->StereoUpdate(); // Marine, besoin d'une update? Si oui où? 
-  renderWindow->AddRenderer(renderer);
-  renderWindow->SetWindowName("CrystalEyes");
+    // The render window is the actual GUI window
+    // that appears on the computer screen
+    vtkNew<vtkRenderWindow> renderWindow;
+    renderWindow->SetStereoType(1);    //Marine, c'est cette fonction qu'il fqut utiliser elle est dans vtkRenderWindow.cxx et non celle ci SetStereoTypeToCrystalEyes aui est dans le header
+    renderWindow->SetStereoCapableWindow(1);     //Marine, 1 = crystal eyes
+    renderWindow->SetStereoRender(1);     //Marine,  1 = crystal eyes
+    renderWindow->SetSize(300, 300);
+    renderWindow->AddRenderer(renderer);
+    renderWindow->SetWindowName(renderWindow->GetStereoTypeAsString());
+    renderWindow->StereoUpdate(); // Marine, besoin d'une update? Si oui où?
 
-  // The render window interactor captures mouse events
-  // and will perform appropriate camera or actor manipulation
-  // depending on the nature of the events.
-  vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
-  renderWindowInteractor->SetRenderWindow(renderWindow);
+    //Marine, pour savoir quel type de stereo on utilise
+    cout << "Type de render : " << renderWindow->GetStereoTypeAsString()<< endl;
 
-  // This starts the event loop and as a side effect causes an initial render.
-  renderWindow->Render();
-  renderWindowInteractor->Start();
+    // The render window interactor captures mouse events
+    // and will perform appropriate camera or actor manipulation
+    // depending on the nature of the events.
+    vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+    renderWindowInteractor->SetRenderWindow(renderWindow);
 
-  return EXIT_SUCCESS;
+    // This starts the event loop and as a side effect causes an initial render.
+     
+    renderWindow->Render();
+    renderWindowInteractor->Start();
+
+    return EXIT_SUCCESS;
 }
