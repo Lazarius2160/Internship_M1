b3b213cb3de60a452e3a4b933558cbe020b33c79
Lazarius2160 <76939787+Lazarius2160@users.noreply.github.com>
2021-06-10 17:26:43 +0200

    CODE_QUADB: access renderwindow

--- WidgetNewLayout.cxx
+++ WidgetNewLayout.cxx
@@ -30,68 +30,71 @@
 #include <vtkMRMLScene.h> //marine, include pour les nodes et la scene
 #include <vtkMRMLViewLogic.h> //marine, include pour les nodes et la scene
 #include <vtkMRMLViewNode.h> //marine, include pour les nodes et la scene
+#include <qMRMLThreeDWidget.h>
 
 
 int main(int argc, char** argv)
 {
 
-  //marine, test creer un widget en stereo hors du layout
-  
-  //marine, LAYOUT
-  string layoutName("Test3DView");
-  string layoutLabel("QB");
-  float layoutColor[3] = { 0.0, 0.0, 1.0 };
+	//marine, test creer un widget en stereo hors du layout
+
+	//marine, LAYOUT
+	string layoutName("Test3DView");
+	string layoutLabel("QB");
+	float layoutColor[3] = { 0.0, 0.0, 1.0 };
+
+	// marine, ownerNode manages this view instead of the layout manager(it can be any node in the scene)
+	vtkNew<vtkMRMLScene> viewOwnerNode;
+	viewOwnerNode->AddNewNodeByClass("vtkMRMLQuadBufferModuleNode");
+
+	//marine, Create MRML NODE
+	vtkNew<vtkMRMLViewLogic> viewLogic;
+	viewLogic->setMRMLScene(q->mrmlScene()); //marine, pk q dans le module view controller??
+
+	vtkNew<vtkMRMLViewNode> viewNode;
+	viewNode.viewLogic->AddViewNode(layoutName); //marine , structure de ce truc??
+	viewNode->SetLayoutLabel(layoutLabel);  //marine,trouve ses fonctions dans son heritage
+	viewNode->SetLayoutColor(layoutColor);
+	viewNode->SetAndObserveParentLayoutNodeID(viewOwnerNode.GetID()); //marine, structure?
+	viewNode->SetStereoType(1); // on active le mode stereo dans le noeud, ne sait pas si ca sert
+
+	//marine, pas besoin de app?? QApplication app(argc, argv);
+
+
+	// marine, SURFACE
+	// marine, By default setstereo is OFF so quadbuffering is not possible https://doc.qt.io/qt-5/qsurfaceformat.html, voir dans QVTKOpenGLWindow.h ouis qsurfqceformat
+	QSurfaceFormat format;
+	format.setFormat(format);
+
+	//WIDGET
+	// pour palier au manque de : QVTKOpenGLStereoWidget,  on créer un widget slicer classique et on lui ajoute la render window
+	qMRMLThreeDWidget viewWidget;
+	viewWidget->setQuadBufferStereoSupportEnabled(1);
+	viewWidget->SetMRMLScene(q->mrmlScene()); //marine, voir pk cette structure
+	viewWidget->SetMRMLViewNode(viewNode);
+	// traduire ca en c++ : slicer.app.layoutManager().threeDWidget(0).threeDView().renderWindow().SetStereoType(1)
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! accede à la renderwindow depuis threedview car herite de ctkVTKRenderView qui herite de ctkVTKAbstractView 
+	viewWiget.threeDView().renderWindow
 	
-  // marine, ownerNode manages this view instead of the layout manager(it can be any node in the scene)
-  vtkNew<vtkMRMLScene> viewOwnerNode;
-  viewOwnerNode->AddNewNodeByClass("vtkMRMLQuadBufferModuleNode");
 
-  //marine, Create MRML NODE
-  vtkNew<vtkMRMLViewLogic> viewLogic; 
-  viewLogic->setMRMLScene(q->mrmlScene()); //marine, pk q dans le module view controller??
-
-  vtkNew<vtkMRMLViewNode> viewNode;
-  viewNode.viewLogic->AddViewNode(layoutName); //marine , structure de ce truc??
-  viewNode->SetLayoutLabel(layoutLabel);  //marine,trouve ses fonctions dans son heritage
-  viewNode->SetLayoutColor(layoutColor);
-  viewNode->SetAndObserveParentLayoutNodeID(viewOwnerNode.GetID()); //marine, structure?
-  viewNode->SetStereoType(1); // on active le mode stereo dans le noeud, ne sait pas si ca sert
-
-  //marine, pas besoin de app?? QApplication app(argc, argv);
-  
-
-  // marine, WIDGET
-  // marine, By default setstereo is OFF so quadbuffering is not possible https://doc.qt.io/qt-5/qsurfaceformat.html, voir dans QVTKOpenGLWindow.h ouis qsurfqceformat
-  QSurfaceFormat::setDefaultFormat(QVTKOpenGLStereoWidget::defaultFormat(1));
-  //marine, pas eu besoin lors des test : widget.setFormat(format);
-  
-  
-  QVTKOpenGLStereoWidget widget;
-  // marine, du moment que le widget est creer avec e type stereo, si fait une render window en stereo avnt apres pendnat etc affichera correctement en stereo, et sans stereo (mais moche) si le demande pas
-  /*marine, oblige de definir un widget aui accepte la stereo, si pas possible prendre un widget de ce type alors fait :
-  * qMRMLThreeDWidget::setQuadBufferStereoSupportEnabled ??? OU
-  * qMRMLThreeDViewControllerWidget::setQuadBufferStereoSupportEnabled ??? OU/ET
-  * qMRMLThreeDViewControllerWidget::setStereoType
-  */
-
-  //marine, RENDER WINDOW ??????????????????????????????????????????
-  vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
-  //marine, need to set the stereo type before creating the render window
-  renderWindow->SetStereoType(1);
-  renderWindow->SetStereoCapableWindow(1);
-  renderWindow->SetStereoRender(1);
-  widget.setRenderWindow(renderWindow); 
-  widget.resize(600, 600);
-  widget.renderWindow()->StereoUpdate();
-  renderWindow->Render();  
-  //?????????????????????
+	//marine, RENDER WINDOW ??????????????????????????????????????????
+	//vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
+	//marine, need to set the stereo type before creating the render window
+	renderWindow->SetStereoType(1);
+	renderWindow->SetStereoCapableWindow(1);
+	renderWindow->SetStereoRender(1);
+	widget.setRenderWindow(renderWindow);
+	widget.resize(600, 600);
+	widget.renderWindow()->StereoUpdate();
+	renderWindow->Render();
+	//?????????????????????
 
 
 
 
-  widget.show();
+	widget.show();
 
-  //marine, pas besoin de app : app.exec();
+	//marine, pas besoin de app : app.exec();
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
