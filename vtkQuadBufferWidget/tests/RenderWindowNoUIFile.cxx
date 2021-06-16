86a2cc9943c2c62e1e90f75ede1b2bd119f2b1be
Lazarius2160 <76939787+Lazarius2160@users.noreply.github.com>
2021-06-16 12:03:26 +0200

    CODE_QUADB: test 2 widget one stereo other not
    Success

---
+++ vtkQuadBufferWidget/tests/RenderWindowNoUIFile.cxx
@@ -0,0 +1,133 @@
+// Create a render window inside a QWidget 
+//TESTED TO CREATE 2 DIFFERENT WIDGETS, ONE IN STEREO WITH A STEREO SURFACE AND ONE NATIVE WITH A NATIVE DEFAULT FORMAT TO SEE IF CHANGING THE FORMAT JUST BEFORE SHOWING THE
+// WIDGET HAD AN IMPACT. BOTH APPEAR WELL ON THE SCREEN WITH ONE IN STEREO AND NOT THE OTHER
+
+
+#include <QApplication>
+
+#include <vtkActor.h>
+#include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkSphereSource.h>
+#include <vtkVersion.h>
+
+#if VTK_VERSION_NUMBER >= 89000000000ULL
+#define VTK890 1
+#endif
+// Marine, de ce que j'ai capte grace a mes test avec vtk 9.0 vtk890 = vtk 9 
+
+#include <QSurfaceFormat>
+#include <QVTKOpenGLStereoWidget.h> //marine, on utilise cet version car derniere en date et normalement fonctionne
+#include <QVTKOpenGLNativeWidget.h>
+
+int main(int argc, char** argv)
+{
+
+  QApplication app(argc, argv);
+  
+  //Marine, doit avoir le surface format pret pour la stereo donc utilise :
+    
+  //marine, ou en une ligne : 
+  //QSurfaceFormat::setDefaultFormat(QVTKOpenGLStereoWidget::defaultFormat(1));
+  // marine, test sans stereo widget mais marche pas:  
+  QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat(1));
+  //on place l'autre surface plus bas pour les tests
+
+  QVTKOpenGLStereoWidget widget;
+  QVTKOpenGLNativeWidget widgetNative;
+  // pas utile si passe par stereo widget meme en ++ lignes commande : widget.defaultFormat(1);
+  
+  vtkNew<vtkNamedColors> colors;
+
+  vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
+  vtkNew<vtkGenericOpenGLRenderWindow> renderWindowNative;
+
+#if VTK890
+  //marine, need to set the stereo type before creating the render window
+  renderWindow->SetStereoType(1);
+  renderWindow->SetStereoCapableWindow(1);
+  renderWindow->SetStereoRender(1);
+  renderWindowNative->SetStereoType(1);
+  renderWindowNative->SetStereoCapableWindow(1);
+  renderWindowNative->SetStereoRender(1);
+  widget.setRenderWindow(renderWindow);
+  widgetNative.setRenderWindow(renderWindowNative);
+#else 
+  renderWindow->SetStereoType(1);
+  renderWindow->SetStereoCapableWindow(1);
+  renderWindow->SetStereoRender(1);
+  widget.SetRenderWindow(renderWindow);
+#endif
+
+  widget.resize(600, 600);
+  widgetNative.resize(600, 600);
+
+  vtkNew<vtkSphereSource> sphereSource;
+
+  vtkNew<vtkPolyDataMapper> sphereMapper;
+  sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
+
+  vtkNew<vtkActor> sphereActor;
+  sphereActor->SetMapper(sphereMapper);
+  sphereActor->GetProperty()->SetColor(colors->GetColor4d("Tomato").GetData());
+
+  vtkNew<vtkRenderer> renderer;
+  renderer->AddActor(sphereActor);
+  renderer->SetBackground(colors->GetColor3d("SteelBlue").GetData());
+  //-------------------------------
+  vtkNew<vtkSphereSource> sphereSource2;
+
+  vtkNew<vtkPolyDataMapper> sphereMapper2;
+  sphereMapper2->SetInputConnection(sphereSource2->GetOutputPort());
+
+  vtkNew<vtkActor> sphereActor2;
+  sphereActor2->SetMapper(sphereMapper2);
+  sphereActor2->GetProperty()->SetColor(colors->GetColor4d("Tomato").GetData());
+
+  vtkNew<vtkRenderer> renderer2;
+  renderer2->AddActor(sphereActor2);
+  renderer2->SetBackground(colors->GetColor3d("SteelBlue").GetData());
+
+
+#if VTK890
+  widget.renderWindow()->AddRenderer(renderer);
+  widget.renderWindow()->SetWindowName(widget.renderWindow()->GetStereoTypeAsString()); // Marine, get viens du vtkrenderwindow
+  widget.renderWindow()->StereoUpdate();
+
+  widgetNative.renderWindow()->AddRenderer(renderer2);
+  widgetNative.renderWindow()->SetWindowName(widgetNative.renderWindow()->GetStereoTypeAsString()); // Marine, get viens du vtkrenderwindow
+  widgetNative.renderWindow()->StereoUpdate();
+  //cout << "Type de render : " << widget.renderWindow()->GetStereoTypeAsString() << endl;
+  //cout << "VTK version 9" << endl;
+  renderWindow->Render();
+  renderWindowNative->Render();
+  
+#else
+  widget.GetRenderWindow()->AddRenderer(renderer);
+  widget.GetRenderWindow()->SetWindowName(widget.GetRenderWindow()->GetStereoTypeAsString());
+  widget.GetRenderWindow()->StereoUpdate();
+  //cout << "Type de render : " << widget.GetRenderWindow()->GetStereoTypeAsString() << endl;
+  //cout << "vtk autre version" << endl;
+  renderWindow->Render();
+
+#endif
+
+
+  widgetNative.show();
+
+  QSurfaceFormat format;
+  format.setStereo(true);
+  widget.setFormat(format);
+
+  widget.show();
+
+  app.exec();
+
+
+  return EXIT_SUCCESS;
+}
