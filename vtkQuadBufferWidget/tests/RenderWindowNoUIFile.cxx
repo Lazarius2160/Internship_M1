7a2c1b607e2f077ac35d7fd75b1b860649a8be98
Lazarius2160 <76939787+Lazarius2160@users.noreply.github.com>
2021-06-16 12:35:10 +0200

    CODE_QUADB: 2 widgets many tests

--- vtkQuadBufferWidget/tests/RenderWindowNoUIFile.cxx
+++ vtkQuadBufferWidget/tests/RenderWindowNoUIFile.cxx
@@ -1,7 +1,6 @@
 // Create a render window inside a QWidget 
-//TESTED TO CREATE 2 DIFFERENT WIDGETS, ONE IN STEREO WITH A STEREO SURFACE AND ONE NATIVE WITH A NATIVE DEFAULT FORMAT TO SEE IF CHANGING THE FORMAT JUST BEFORE SHOWING THE
-// WIDGET HAD AN IMPACT. BOTH APPEAR WELL ON THE SCREEN WITH ONE IN STEREO AND NOT THE OTHER
-
+// TESTED TO CREATE 2 DIFFERENT WIDGETS, ONE IN STEREO WITH A STEREO SURFACE AND ONE NATIVE WITH A NATIVE DEFAULT FORMAT TO SEE IF CHANGING THE FORMAT JUST BEFORE SHOWING THE
+// WIDGET HAD AN IMPACT. SEE THE TESTS ON THE GRID IN ONE NOTE
 
 #include <QApplication>
 
@@ -33,14 +32,12 @@ int main(int argc, char** argv)
   //Marine, doit avoir le surface format pret pour la stereo donc utilise :
     
   //marine, ou en une ligne : 
-  //QSurfaceFormat::setDefaultFormat(QVTKOpenGLStereoWidget::defaultFormat(1));
-  // marine, test sans stereo widget mais marche pas:  
+  // si met (0) lors active pas le mode stereo de base doit le reactiver apres
+  //QSurfaceFormat::setDefaultFormat(QVTKOpenGLStereoWidget::defaultFormat(1));  
   QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat(1));
-  //on place l'autre surface plus bas pour les tests
 
   QVTKOpenGLStereoWidget widget;
-  QVTKOpenGLNativeWidget widgetNative;
-  // pas utile si passe par stereo widget meme en ++ lignes commande : widget.defaultFormat(1);
+  QVTKOpenGLNativeWidget widgetNative; // ne supporte pas le stereo de base!
   
   vtkNew<vtkNamedColors> colors;
 
@@ -74,7 +71,7 @@ int main(int argc, char** argv)
 
   vtkNew<vtkActor> sphereActor;
   sphereActor->SetMapper(sphereMapper);
-  sphereActor->GetProperty()->SetColor(colors->GetColor4d("Tomato").GetData());
+  sphereActor->GetProperty()->SetColor(colors->GetColor4d("LimeGreen").GetData());
 
   vtkNew<vtkRenderer> renderer;
   renderer->AddActor(sphereActor);
@@ -117,17 +114,14 @@ int main(int argc, char** argv)
 
 #endif
 
+  /*QSurfaceFormat format;
+  format.setStereo(true);
+  widget.setFormat(format);*/
 
   widgetNative.show();
-
-  QSurfaceFormat format;
-  format.setStereo(true);
-  widget.setFormat(format);
-
   widget.show();
 
   app.exec();
-
 
   return EXIT_SUCCESS;
 }
