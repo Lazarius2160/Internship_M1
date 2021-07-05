8b6a94389d246b0b7efb236e854ecc716f5738c2
Lazarius2160 <marine.camba@etu.imt-lille-douai.fr>
2021-07-05 11:02:27 +0200

    CODE_QUADB: put setter before adding node to widget

--- myFirstExtension/myFirstModule/myFirstModule.py
+++ myFirstExtension/myFirstModule/myFirstModule.py
@@ -16,16 +16,12 @@ class mySecondModule(ScriptedLoadableModule):
 
   def __init__(self, parent):
     ScriptedLoadableModule.__init__(self, parent)
-    self.parent.title = "mySecondModule"  # TODO: make this more human readable by adding spaces
-    self.parent.categories = ["Examples"]  # TODO: set categories (folders where the module shows up in the module selector)
-    self.parent.dependencies = []  # TODO: add here list of module names that this module requires
-    self.parent.contributors = ["John Doe (AnyWare Corp.)"]  # TODO: replace with "Firstname Lastname (Organization)"
-    # TODO: update with short description of the module and a link to online module documentation
+    self.parent.title = "mySecondModule"  
+    self.parent.categories = ["Test"]  
+    self.parent.dependencies = []  
+    self.parent.contributors = ["Marine CAMBA - CENIR Paris Brain Institute, Sara FERNANDEZ VIDAL - CENIR Paris Brain Institute"]  
     self.parent.helpText = """
-This is an example of scripted loadable module bundled in an extension.
-See more information in <a href="https://github.com/organization/projectname#mySecondModule">module documentation</a>.
-"""
-    # TODO: replace with organization, grant and thanks
+This module allows user to display in QuadBuffer Stereo mode in Slicer 4.13 using VTK 9"""
     self.parent.acknowledgementText = """
 This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc., Andras Lasso, PerkLab,
 and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
@@ -64,45 +60,40 @@ class mySecondModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
     # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
     # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
-    # "setMRMLScene(vtkMRMLScene*)" slot.
     uiWidget.setMRMLScene(slicer.mrmlScene)
     self.setupQuadBufferMode()
 
   def setupQuadBufferMode(self):
-    # layout name is used to create and identify the underlying view node and  should be set to a value that is not used in any of the layouts owned by the layout manager
+
     layoutName = "QuadBuffered window"
     layoutLabel = "QB"
     layoutColor = [0.5, 0.5, 1.0]
     # ownerNode manages this view instead of the layout manager (it can be any node in the scene)
     self.viewOwnerNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScriptedModuleNode")
 
-    # Create MRML node
     self.viewLogic = slicer.vtkMRMLViewLogic()
     self.viewLogic.SetMRMLScene(slicer.mrmlScene)
     self.viewNode = self.viewLogic.AddViewNode(layoutName)
     self.viewNode.SetLayoutLabel(layoutLabel)
     self.viewNode.SetLayoutColor(layoutColor)
     self.viewNode.SetAndObserveParentLayoutNodeID(self.viewOwnerNode.GetID())    
-    # DOIT DIRE QUE LA WINDOW EST STEREO CAPABLE AVANT DE LUI AFFECTER LE NOEUD CAR AVANT LE RENDER (logique ou pas du tout?)
-    #attention a quelle render window on touche, normalement celle de viewWidget donc OK 
+    # self.viewNode.SetStereoType(3) #active le mode stereo sur l'image + dans le menu déroulant, cad comme si clique moi sur le bouton, 3=quadbuffer
+
     self.viewWidget = slicer.qMRMLThreeDWidget() 
-    self.renderWindowQuadBuffer = self.viewWidget.threeDView().renderWindow()
-    self.renderWindowQuadBuffer.SetStereoCapableWindow(1)  #on off if the window is created in stereo capable mode
-    self.viewNode.SetStereoType(3) #active le mode stereo sur l'image + dans le menu déroulant, cad comme si clique moi sur le bouton, 3=quadbuffer
-
-    # Create widget
-
+    # self.viewWidget.setFormat(self.setStrereo(True)) #passe au format stereo, voit si bonne forme pour le code
     # self.viewWidget.setQuadBufferStereoSupportEnabled(1)
+
+    self.renderWindowQuadBuffer = self.viewWidget.threeDView().renderWindow()
+    self.renderWindowQuadBuffer.SetStereoCapableWindow(1)  #on off if the window is created in stereo capable mode, needs to be stereowidget
+    self.renderWindowQuadBuffer.SetStereoType(1) #1=crystaleyes, 2=red blue
+    #self.renderWindowQuadBuffer.SetStereoRender(1)  #on off for stereo rendering
+
     self.viewWidget.setMRMLScene(slicer.mrmlScene)
     self.viewWidget.setMRMLViewNode(self.viewNode)
     self.viewWidget.resize(800, 800)
-    # self.viewWidget.setFormat(self.setStrereo(True)) #passe au format stereo, voit si bonne forme pour le code
+
     
     #Modify render window
-
-    self.renderWindowQuadBuffer.SetStereoType(1) #1=crystaleyes, 2=red blue
-
-    #self.renderWindowQuadBuffer.SetStereoRender(1)  #on off for stereo rendering
     # comme on créer la fenetre a partir de view widget surement pas besoin de ces 3 lignes :
     #self.viewWidget.setRenderWindow(self.renderWindowQuadBuffer)
     #self.viewWidget.renderWindowQuadBuffer.StereoUpdate()
