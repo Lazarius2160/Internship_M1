7e5994ef6264fcc46e62c8e45b9391e9c01ba2b7
Lazarius2160 <marine.camba@etu.imt-lille-douai.fr>
2021-07-09 15:38:46 +0200

    CODE_QUADB: tested on Slicer 4.11 release and 4.13 (bug 3D)

--- myFirstExtension/myFirstModule/myFirstModule.py
+++ myFirstExtension/myFirstModule/myFirstModule.py
@@ -5,6 +5,7 @@ import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
 from slicer.util import VTKObservationMixin
 
+
 #
 # mySecondModule
 #
@@ -13,6 +14,9 @@ class mySecondModule(ScriptedLoadableModule):
   """Uses ScriptedLoadableModule base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
+  # Depending on which VTK to use, needs to have slighly different methods
+  #if (VTK_VERSION_NUMBER >= 89000000000ULL):
+  #  VTK890 = 1;
 
   def __init__(self, parent):
     ScriptedLoadableModule.__init__(self, parent)
@@ -57,58 +61,53 @@ class mySecondModuleWidget(ScriptedLoadableModuleWidget):
     self.layout.addWidget(uiWidget)
     self.ui = slicer.util.childWidgetVariables(uiWidget)
 
-    # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
-    # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
     uiWidget.setMRMLScene(slicer.mrmlScene)
     self.setupQuadBufferMode()
 
   def setupQuadBufferMode(self):
 
+    #self.format = qt.QSurfaceFormat().setStereo(True)
+
     layoutName = "QuadBuffered window"
     layoutLabel = "QB"
     layoutColor = [0.5, 0.5, 1.0]
-    # ownerNode manages this view instead of the layout manager (it can be any node in the scene)
+
     self.viewOwnerNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScriptedModuleNode")
 
     self.viewLogic = slicer.vtkMRMLViewLogic()
     self.viewLogic.SetMRMLScene(slicer.mrmlScene)
+
     self.viewNode = self.viewLogic.AddViewNode(layoutName)
     self.viewNode.SetLayoutLabel(layoutLabel)
     self.viewNode.SetLayoutColor(layoutColor)
     self.viewNode.SetAndObserveParentLayoutNodeID(self.viewOwnerNode.GetID())    
 
-    # self.stereoWidget = QVTKOpenGLWidget()  si on ne peut pas avoir le stereo widget par defaut > syntaxe affreuse chanegemet de plan
     self.viewWidget = slicer.qMRMLThreeDWidget() 
-    # self.viewWidget.setFormat(self.setStrereo(True)) #passe au format stereo, voit si bonne forme pour le code
-    self.viewWidget.setQuadBufferStereoSupportEnabled(1)
+    #self.viewWidget.setFormat(format) #passe au format stereo
 
     self.renderWindowQuadBuffer = self.viewWidget.threeDView().renderWindow()
-    self.renderWindowQuadBuffer.SetStereoCapableWindow(1)  #on off if the window is created in stereo capable mode, needs to be stereowidget
+    #self.renderWindowQuadBuffer.SetStereoCapableWindow(1)  #on off if the window is created in stereo capable mode, needs to be stereowidget car si l'est pas print 
+    #"Stereo Capable Window Requested: No" et si l'est direct yes donc peut etre pqs besoin de cette ligne
     #self.renderWindowQuadBuffer.SetStereoType(2) #1=crystaleyes, 2=red blue  >> seul ne se passe rien doit ajouter autre chose
-    self.renderWindowQuadBuffer.SetStereoRender(1)  #on off for stereo rendering
+    #self.renderWindowQuadBuffer.SetStereoRender(1)  #on off for stereo rendering
 
-    self.viewNode.SetStereoType(3) #active le mode stereo sur l'image + dans le menu déroulant, cad comme si clique moi sur le bouton, 3=quadbuffer
+    #self.viewNode.SetStereoType(3) #active le mode stereo sur l'image + dans le menu déroulant, cad comme si clique moi sur le bouton, 3=quadbuffer
     self.viewWidget.setMRMLScene(slicer.mrmlScene)
     self.viewWidget.setMRMLViewNode(self.viewNode)
     self.viewWidget.resize(800, 800)
 
     
-    #Modify render window
-    # comme on créer la fenetre a partir de view widget surement pas besoin de ces 3 lignes :
-    #self.viewWidget.setRenderWindow(self.renderWindowQuadBuffer)
-    #self.renderWindowQuadBuffer.Render()
-    
     self.ui.EnableQuadBufferButton.connect('clicked(bool)', self.showQuadBufferWidget)
 
 
   def showQuadBufferWidget(self):
-    # self.renderWindowQuadBuffer.StereoUpdate() ici ou dans la fonction precedente fait crash l'appli
     self.viewWidget.show()
     # tests
-    print(self.viewNode.GetStereoType()) #Not available as string only int
-    print(self.renderWindowQuadBuffer.GetStereoTypeAsString())
+    print("Render window")
+    print(self.renderWindowQuadBuffer)
+    print("View node")
+    print(self.viewNode)
   
-
 
 
 
